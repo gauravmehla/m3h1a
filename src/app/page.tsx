3310c54232fe51a646e93ef5d015b68635df9077
Gaurav Mehla <gaurav@mehla.in>
2024-04-16 16:21:34 -0700

    Day 0: Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,17 @@ export default function Home() {
           <code className="font-mono font-bold">Gaurav Mehla</code>
         </p>
       </div>
+
+      <Image
+        src="/v0/b/m3h1a-1n.appspot.com/o/mehla.in%2Fstatic%2Fimg%2Fmehla_logo.JPG?alt=media&token=83473051-7b21-4704-8834-c7adbae88098"
+        alt="Gaurav Mehla"
+        width={400}
+        height={400}
+        className="rounded-full"
+        style={{
+          margin: "auto",
+        }}
+      />
     </main>
   );
 }
