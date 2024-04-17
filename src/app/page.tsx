df3d704d8062aa5e0e0b6d5a0ac463912e5c0251
Gaurav Mehla <gaurav@mehla.in>
2024-04-16 21:34:03 -0700

    Created first component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import IntroImg from "./components/main-intro";
 
 export default function Home() {
   return (
@@ -10,16 +10,7 @@ export default function Home() {
         </p>
       </div>
 
-      <Image
-        src="/v0/b/m3h1a-1n.appspot.com/o/mehla.in%2Fstatic%2Fimg%2Fmehla_logo.JPG?alt=media&token=83473051-7b21-4704-8834-c7adbae88098"
-        alt="Gaurav Mehla"
-        width={400}
-        height={400}
-        className="rounded-full"
-        style={{
-          margin: "auto",
-        }}
-      />
+      <IntroImg />
     </main>
   );
 }
