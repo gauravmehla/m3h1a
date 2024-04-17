f67588b5b122ecdbcaee5642cd823f555994bdc3
Gaurav Mehla <gaurav@mehla.in>
2024-04-16 21:34:03 -0700

    Created first component

---
+++ src/app/components/main-intro.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function CustomImage() {
+  return (
+    <Image
+      src="/v0/b/m3h1a-1n.appspot.com/o/mehla.in%2Fstatic%2Fimg%2Fmehla_logo.JPG?alt=media&token=83473051-7b21-4704-8834-c7adbae88098"
+      alt="Gaurav Mehla"
+      width={400}
+      height={400}
+      className="rounded-full"
+      style={{
+        margin: "auto",
+      }}
+    />
+  )
+}
