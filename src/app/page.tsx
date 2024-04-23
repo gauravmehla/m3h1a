9fef4e3523c4109277aea3f4eba4135258f993b1
Gaurav Mehla <gaurav@mehla.in>
2024-04-23 14:02:41 -0700

    dark mode done. Styles created. font imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect, useRef } from "react";
 import { useTheme } from "next-themes";
+import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
+import "@/app/styles/fonts.css";
 
 export default function Home() {
   const [messages, setMessages] = useState<{ sender: string; text: string }[]>(
@@ -9,7 +11,8 @@ export default function Home() {
   );
   const [input, setInput] = useState("");
   const messagesEndRef = useRef(null);
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
 
   const handleSend = (e: React.FormEvent) => {
     e.preventDefault();
@@ -19,7 +22,7 @@ export default function Home() {
   };
 
   const handleThemeChange = () => {
-    setTheme(theme === "light" ? "dark" : "light");
+    setTheme(resolvedTheme === "light" ? "dark" : "light");
   };
 
   useEffect(() => {
@@ -29,25 +32,46 @@ export default function Home() {
     }
   }, [messages]);
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <div className="flex flex-col h-screen justify-between">
       <button
         onClick={handleThemeChange}
-        className="self-end m-4 p-2 bg-blue-500 text-white rounded"
+        className="self-end m-4 p-2 bg-blue-500 text-white rounded flex items-center"
       >
-        Switch Theme
+        {resolvedTheme === "dark" ? (
+          <MdOutlineLightMode key="light" />
+        ) : (
+          <MdOutlineDarkMode key="dark" />
+        )}
+        <span className="ml-2">
+          {resolvedTheme === "dark" ? "Light Mode" : "Dark Mode"}
+        </span>
       </button>
       <div className="overflow-auto p-4">
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`flex ${message.sender === "me" ? "justify-end" : ""}`}
-          >
-            <div className="m-4 p-2 rounded text-[var(--color-text)] bg-[var(--color-background)] inline-block">
-              {message.text} {message.sender}
-            </div>
+        {messages.length === 0 ? (
+          <div className="flex items-center justify-center h-screen text-2xl big-center-text">
+            Hi, Gaurav This side.
           </div>
-        ))}
+        ) : (
+          messages.map((message, index) => (
+            <div
+              key={index}
+              className={`flex ${message.sender === "me" ? "justify-end" : ""}`}
+            >
+              <div className="m-4 p-2 rounded text-[var(--color-text)] bg-[var(--color-background)] inline-block">
+                {message.text} {message.sender}
+              </div>
+            </div>
+          ))
+        )}
         <div ref={messagesEndRef} />
       </div>
       <form onSubmit={handleSend} className="m-2 flex px-10 items-center">
@@ -55,8 +79,8 @@ export default function Home() {
           type="text"
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          className="flex-grow p-2 border rounded text-black"
-          placeholder="Type a message"
+          className="flex-grow p-2 border rounded text-[var(--color-text)]"
+          placeholder="What you want to know about me?"
         />
         <button
           type="submit"
