9c269f7177a7b98fb913a39e9bf19fbd3aa88cea
gaurav.ror007 <gaurav.ror007@gmail.com>
2024-05-18 23:41:22 +0000

    idx configured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,7 +132,7 @@ export default function Home() {
       <div className="overflow-auto p-4">
         {messages.length === 0 ? (
           <div className="flex items-center justify-center text-2xl big-center-text typewriter">
-            Hi, Gaurav This side.
+            Hi, gauravmehla This side. 👋
           </div>
         ) : (
           messages.map((message, index) => (
